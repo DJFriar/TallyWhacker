249f00b30728bee27f9ef27693425970af487ff0
DJFriar <me@tommyc.net>
2020-06-17 23:31:34 -0500

    Fixed a missing environmentObject that was causing the canvas to fail.

--- Tally Whacker/ContentView.swift
+++ Tally Whacker/ContentView.swift
@@ -114,6 +114,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environmentObject(BonusStats())
     }
 }
